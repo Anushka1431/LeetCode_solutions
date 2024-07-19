d4494e2cc7382d1180bfe586055b23c261968074
Anushka1431 <121926823+Anushka1431@users.noreply.github.com>
2024-07-19 12:29:31 +0530

    Time: 1 ms (98.96%), Space: 41.4 MB (40.84%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,34 @@
+//add all the paranthesis to the stack and then once we have the closing ones cancel the pairs one by one .
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' ||ch=='{'||ch=='['){
+                stack.push(ch);
+            }
+            else{
+                if(ch==')'){
+                if(stack.isEmpty() || stack.pop()!='('){
+                    return false;
+                }
+                
+            }
+            if(ch=='}'){
+                if(stack.isEmpty() || stack.pop()!='{'){
+                    return false;
+                }
+                
+            }
+            if(ch==']'){
+                if(stack.isEmpty() || stack.pop()!='['){
+                    return false;
+                }
+                
+            }
+            }
+            
+        }
+        return stack.isEmpty();
+    }
+}
